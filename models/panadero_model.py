40c2cde34a4b669b94e88d06025aeaa885e81ffb
Carlos Reyes <caereyes0@gmail.com>
2024-12-02 00:04:51 -0400

    Panadero

--- models/panadero_model.py
+++ models/panadero_model.py
@@ -9,14 +9,18 @@ class Panadero(UserMixin, db.Model):
     apellido_paterno = db.Column(db.String(50), nullable = False)
     apellido_materno = db.Column(db.String(50), nullable = False)
     contrasenia = db.Column(db.String(300), nullable = False)
+    sueldo = db.Column(db.Double(), nullable = False)
     rol = db.Column(db.String(50), nullable = False, default = "panadero")
+    fecha_nacimiento = db.Column(db.DateTime, nullable = False)
     
-    def __init__(self,nombre, apellido_paterno, apellido_materno, contrasenia,rol="panadero"):
+    def __init__(self,nombre, apellido_paterno, apellido_materno, contrasenia, sueldo,fecha_nacimiento, rol="panadero"):
         self.nombre = nombre
         self.apellido_paterno = apellido_paterno
         self.apellido_materno = apellido_materno
         self.contrasenia = generate_password_hash(contrasenia)
         self.rol = rol
+        self.sueldo = sueldo
+        self.fecha_nacimiento = fecha_nacimiento
     
     def guardar(self):
         db.session.add(self)
