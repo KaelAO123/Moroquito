1067c3087f3b524e63d35a05d046ee964c270241
Carlos Reyes <caereyes0@gmail.com>
2024-12-05 11:28:02 -0400

    Comentarios - Random

--- run.py
+++ run.py
@@ -1,49 +1,34 @@
 from flask import Flask
 from flask_login import LoginManager
 
-# Importamos el controlador de usuarios
 from controllers import usuario_controller
 
-# Importamos el controlador de animales
 from controllers import producto_controller
 
-# Importamos el controlador de panaderos
 from controllers import pedido_controller
 
-# Importamos la base de datos
 from database import db
 from models.usuario_model import Usuarios
 
-# Inicializa la aplicación Flask
 app = Flask(__name__)
-# Configuración de la base de datos
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Moroquito.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "clave-secreta"
-# Configuración de Flask-Login
 login_manager = LoginManager()
-# Especifica la ruta de inicio de sesión
 login_manager.login_view = "user.login"
 login_manager.init_app(app)
 
-
-
-
-# Función para cargar un usuario basado en su ID
 @login_manager.user_loader
 def load_user(user_email):
     return Usuarios.query.get((user_email))
 
-    
-# Inicializa `db` con la aplicación Flask
 db.init_app(app)
-# Registra el Blueprint de usuarios
 app.register_blueprint(usuario_controller.user_bp)
 app.register_blueprint(producto_controller.product_bp)
 app.register_blueprint(pedido_controller.pedido_bp)
 
 if __name__ == "__main__":
-    # Crea las tablas si no existen
+
     with app.app_context():
         db.create_all()
     app.run(debug=True)
--- views/producto_view.py
+++ views/producto_view.py
@@ -2,8 +2,6 @@ from flask import render_template
 from flask_login import current_user
 
 
-# La función `list_animals` recibe una lista de
-# animales y renderiza el template `animales.html`
 def list_products(products):
     return render_template(
         "products.html",
@@ -12,16 +10,11 @@ def list_products(products):
         current_user=current_user,
     )
 
-# La función `create_animal` renderiza el
-# template `create_animal.html` o devuelve un JSON
-# según la solicitud
 def create_product():
     return render_template(
         "create_product.html", title="Crear Producto", current_user=current_user
     )
 
-# La función `update_dulce` recibe un dulce
-# y renderiza el template `update_dulce.html`
 def update_product(product):
     return render_template(
         "update_product.html",
